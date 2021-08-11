33c3b1bf3c00080e6e2662b0ded4fcdedcefbf97
naranjargal baljmaa <naranjargal123@gmail.com>
2021-08-11 20:44:02 +1000

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
-def primary():
-  print("Keep it logically awesome.")
+#def primary():
+  #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[0])
+  print(quotes)
 
-if __name__== "__main__":
-  primary()
+#if __name__== "__main__":
+  #primary()
